35c09529579e32b4d3d96bdd0143991f227b1cbc
Abdul Wahab <aws18320@gmail.com>
2023-03-06 22:54:02 +0500

    First Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,10 @@
 import './App.css';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import Menu from './components/Menu';
+
 import Home from './pages/Home';
-import About from './pages/About';
+import { About } from './pages/About';
 import Customorder from './pages/Customorder';
-import Contact from './pages/Contact';
-import Footer from './components/Footer';
-import ProductDetail from './components/ProductDetail';
-import MainCategory from './pages/MainCategory';
-import SubCategory from './components/SubCategory';
-import Dashboard from './admin/Dashboard';
-import Login from './admin/Login';
-import { NewProduct } from './admin/NewProduct';
-import { Thankyou } from './components/Thankyou';
-import { UpdateProduct } from './admin/UpdateProduct';
+import { Contact } from './pages/Contact';
 import { ProductContext } from './context/ProductContext';
 import { useState, useEffect } from 'react';
 import axios from 'axios';
@@ -44,24 +35,20 @@ function App() {
 		<BrowserRouter>
 			<ProductContext.Provider value={{ data }}>
 				<div id="container">
-					<Menu />
 					<div id="main-content">
 						<Routes>
-							<Route path="/admin" element={user ? <Dashboard /> : <Navigate to={'/login'} />} />
-							<Route path="/login" element={!user ? <Login /> : <Navigate to={'/admin'} />} />
-							<Route path="/addproduct" element={user ? <NewProduct /> : <Navigate to={'/login'} />} />
-							<Route path="/update/:id" element={user ? <UpdateProduct /> : <Navigate to={'/login'} />} />
 							<Route path="/" exact element={<Home />} />
 							<Route path="/about" element={<About />} />
 							<Route path="/contact" element={<Contact />} />
+							{/*
 							<Route path="/:category" element={<MainCategory />} />
 							<Route path="/:category/:subcategory" element={<SubCategory />} />
 							<Route path="/:category/:subcategory/:productId" exact element={<ProductDetail />} />
+
+*/}
 							<Route path="/customorder" element={<Customorder />} />
-							<Route path="/thankyou" element={<Thankyou />} />
 						</Routes>
 					</div>
-					<Footer />
 				</div>
 			</ProductContext.Provider>
 		</BrowserRouter>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,24 +1,88 @@
 import React from 'react';
-import Categories from '../components/Categories';
-import HomeExtras from '../components/HomeExtras';
-import Newsletter from '../components/Newsletter';
-import PopularProducts from '../components/PopularProducts';
-import Reviews from '../components/Reviews';
-import CustomOrderButton from '../components/CustomOrderButton';
-import MobileHomeComp from '../components/MobileHomeComp';
+import coverImg from './../images/pentago-cover.jpg';
+import imgOne from './../images/imgOne.jpg';
+import imgTwo from './../images/imgTwo.jpg';
+import imgThree from './../images/imgThree.jpg';
+import imgFour from './../images/imgFour.jpg';
+import imgFive from './../images/imgFive.jpg';
 
-function Home() {
+const homeData = [
+	{
+		id: 1,
+		img: imgOne,
+		title: 'EASE OF ORDERING',
+		descOne: 'We prioritize empathy and effective communication in our work. This approach allows us to deliver a',
+		descTwo: 'seamless sourcing experience for your brand, free of any unnecessary difficulties or complications.',
+	},
+	{
+		id: 2,
+		img: imgTwo,
+		title: 'SAMPLE & MOCKUPS',
+		descOne: 'We encourage new brands to order samples prior bulk production',
+		descTwo: 'to ensure quality and accurate designs.',
+	},
+	{
+		id: 3,
+		img: imgThree,
+		title: 'LOW MOQ',
+		descOne: 'Unlike other apparel suppliers, we have',
+		descTwo: 'LOW MOQ to support small brands at initial stages.',
+	},
+	{
+		id: 4,
+		img: imgFour,
+		title: 'ONE STOP SOLUTION',
+		descOne: 'Fabric sourcing, patterns, sizing, logos, customized packaging & much more.',
+		descTwo: 'All in one clothing factory.',
+	},
+	{
+		id: 5,
+		img: imgFive,
+		title: 'PREMIUM QUALITY',
+		descOne: 'We offer the highest quality. But do not take our word for it.',
+		descTwo: 'Order a sample and see for yourself.',
+	},
+];
+
+const Home = () => {
 	return (
-		<>
-			<MobileHomeComp />
-			<HomeExtras />
-			<Categories />
-			<CustomOrderButton />
-			<PopularProducts />
-			<Reviews />
-			<Newsletter />
-		</>
+		<div className=" bg-stone-900  h-full">
+			<div className="  mx-auto flex flex-col justify-center p-4">
+				<h2 className="text-gray-200 text-3xl font-semibold text-left">WE BUILD</h2>
+				<h2 className="text-red-900 text-3xl font-semibold text-right">YOUR BRAND</h2>
+			</div>
+			<img src={coverImg} className="home-cover" alt="picture of athlete in gym" />
+			<div className="  mx-auto flex flex-col justify-center p-4">
+				<span className="text-gray-200 text-3xl font-semibold text-left "> PAKISTAN'S # 1</span>
+				<span className="text-red-900 text-3xl font-semibold text-center"> CLOTHING</span>
+				<span className="text-gray-200 text-3xl font-semibold text-right"> MANUFACTURER</span>
+			</div>
+			<div className="home-message bg-stone-800 h-full w-5/6 p-4 text-center mx-auto flex flex-col my-4">
+				<span className="text-red-900 text-2xl font-semibold  my-2 ">WE UNDERSTAND.</span>
+
+				<span className="text-gray-200 text-2xl font-semibold mb-16 ">
+					Finding a good apparel manufacturer can be tough. Here's how we can help.{' '}
+				</span>
+			</div>
+			<div className="mx-auto flex flex-col justify-center p-4">
+				{homeData.map((el) => {
+					return (
+						<div className="mx-auto my-4 flex flex-col justify-center w-10/12" key={el.id}>
+							<img src={el.img} className="my-2 rounded-full" />
+							<div className="flex flex-col justify-between text-center">
+								<h2 className=" text-white text-2xl font-semibold my-2">{el.title}</h2>
+								<p className="text-base font-semibold">
+									{' '}
+									<span className="text-gray-200"> {el.descOne} </span>
+									<span className="text-red-900"> {el.descTwo} </span>{' '}
+								</p>
+							</div>
+						</div>
+					);
+				})}
+			</div>
+		</div>
 	);
-}
+};
 
 export default Home;
